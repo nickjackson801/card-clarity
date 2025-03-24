48886dc89ab09c42971596b20de820ca8d13131c
Nick Jackson <nicholascjackson801@gmail.com>
2025-03-23 21:37:57 -0700

    Restore all 13 questions in Quiz component

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -12,22 +12,67 @@ const Quiz = () => {
     {
       id: 'spending',
       question: 'What is your primary spending category?',
-      options: ['Travel', 'Dining', 'Groceries', 'Gas', 'Shopping'],
+      options: ['Travel', 'Dining', 'Groceries', 'Gas', 'Shopping', 'Entertainment', 'Business Expenses'],
     },
     {
       id: 'creditScore',
       question: 'What is your credit score range?',
-      options: ['Excellent (750+)', 'Good (700-749)', 'Fair (650-699)', 'Poor (<650)'],
+      options: ['Excellent (750+)', 'Good (700-749)', 'Fair (650-699)', 'Poor (<650)', 'Not sure'],
     },
     {
       id: 'annualFee',
       question: 'Are you willing to pay an annual fee for better rewards?',
-      options: ['Yes, for better benefits', 'No, prefer no annual fee'],
+      options: ['Yes, up to $95', 'Yes, up to $250', 'Yes, up to $500+', 'No, prefer no annual fee'],
     },
     {
       id: 'rewards',
       question: 'What type of rewards do you prefer?',
-      options: ['Cash Back', 'Travel Miles', 'Points', 'Store Rewards'],
+      options: ['Cash Back', 'Travel Miles', 'Hotel Points', 'Flexible Points', 'Store Rewards'],
+    },
+    {
+      id: 'monthlySpending',
+      question: 'What is your average monthly credit card spending?',
+      options: ['Less than $500', '$500 - $1,000', '$1,000 - $3,000', '$3,000 - $5,000', 'More than $5,000'],
+    },
+    {
+      id: 'travelFrequency',
+      question: 'How often do you travel?',
+      options: ['Rarely', '1-2 times per year', '3-5 times per year', 'Monthly', 'Frequently for business'],
+    },
+    {
+      id: 'diningOut',
+      question: 'How often do you dine out or order takeout?',
+      options: ['Rarely', '1-2 times per week', '3-5 times per week', 'Daily', 'Mostly for business'],
+    },
+    {
+      id: 'grocerySpending',
+      question: 'What is your monthly grocery spending?',
+      options: ['Less than $200', '$200 - $400', '$400 - $800', '$800 - $1,200', 'More than $1,200'],
+    },
+    {
+      id: 'gasSpending',
+      question: 'How much do you spend on gas monthly?',
+      options: ['Less than $100', '$100 - $200', '$200 - $400', 'More than $400', 'I don\'t drive'],
+    },
+    {
+      id: 'onlineShopping',
+      question: 'How often do you shop online?',
+      options: ['Rarely', 'Occasionally', 'Frequently', 'Very Frequently', 'Almost all my shopping is online'],
+    },
+    {
+      id: 'existingCards',
+      question: 'How many credit cards do you currently have?',
+      options: ['None', '1-2 cards', '3-5 cards', '6-8 cards', 'More than 8 cards'],
+    },
+    {
+      id: 'balanceTransfer',
+      question: 'Are you interested in balance transfer options?',
+      options: ['Yes, need to transfer balance', 'Maybe in the future', 'No, not needed'],
+    },
+    {
+      id: 'foreignTransactions',
+      question: 'Do you make international purchases or travel abroad?',
+      options: ['Never', 'Rarely', 'Sometimes', 'Frequently', 'Very Frequently'],
     },
   ];
 
